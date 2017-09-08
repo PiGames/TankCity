0a1e19d8c543f00250c45f3fe75474e560122c58
Condzi <Condzi@users.noreply.github.com>
2017-09-08 20:03:37 +0200

    Settings class works

--- Solution/Testbed/Src/Settings.cpp
+++ Solution/Testbed/Src/Settings.cpp
@@ -21,7 +21,7 @@ static void Init()
 	Context::logger = &logger;
 }
 
-const std::string FILE_PATH = "testSettings.ini";
+const std::string FILE_PATH = "config.ini";
 
 int main()
 {
@@ -30,14 +30,16 @@ int main()
 
 	settings.AddDefault( { "WINDOW", "SIZE_X", "1280" } );
 	settings.AddDefault( { "WINDOW", "SIZE_Y", "720" } );
+	settings.AddDefault( { "WINDOW", "FPS", "60" } );
 
 	settings.Open( FILE_PATH );
 
-	settings.Get( "WINDOW", "SIZE_X" );
-	//LOG_INFO( "WINDOW, SIZE_X, ", settings.Get( "WINDOW", "SIZE_X" ).value().get() );
-	//LOG_INFO( "WINDOW, SIZE_Y, ", settings.Get( "WINDOW", "SIZE_Y" ).value().get() );
+	// ToDo: If cannot find return default - if still cannot find then return noting.
+	LOG_INFO( "WINDOW, SIZE_X, ", settings.Get( "WINDOW", "SIZE_X" ).value().get() );
+	LOG_INFO( "WINDOW, SIZE_Y, ", settings.Get( "WINDOW", "SIZE_Y" ).value().get() );
 	LOG_INFO( "Press ENTER to delete test file ", FILE_PATH );
 
+	settings.Save();
 	std::cin.get();
-	std::experimental::filesystem::remove( { FILE_PATH } );
+	//std::experimental::filesystem::remove( { FILE_PATH } );
 }
